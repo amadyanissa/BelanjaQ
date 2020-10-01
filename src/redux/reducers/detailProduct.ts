c6feff49a002685fe47f9d05ea2d04a9d7156e0e
Amalya Adyanissa <dev@fazzdoc.com>
2020-10-01 14:22:59 +0700

    fix minor bug on reducer add favorite

--- src/redux/reducers/detailProduct.ts
+++ src/redux/reducers/detailProduct.ts
@@ -22,7 +22,15 @@ const detailProductReducer = (state = initialState, actions) => {
     case "ADD_PURCHASE":
       return { ...state, purchased: [...state.purchased, actions.payload] };
     case "ADD_WISHLIST":
-      return { ...state, wishlist: [...state.wishlist, actions.payload] };
+      const check = state.wishlist.filter((el) => {
+        return el.wishList.id == actions.payload.wishList.id;
+      });
+      if (check.length == 0) {
+        return { ...state, wishlist: [...state.wishlist, actions.payload] };
+      } else {
+        return { ...state };
+      }
+
     case "REMOVE_WISHLIST":
       return {
         ...state,
